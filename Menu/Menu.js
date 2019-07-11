2e7a00d9b64d1117e21ad3e2e2218078a33f6f5e
Ricardo <ricksolo88@gmail.com>
2019-07-11 15:38:40 -0500

    Redoing from yesterday

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,27 +9,53 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
 
-  Step 1: Write a function that will create a menu component as seen below:
+function createMenuComponent(menuItems) {
+  const menu = document.createElement('div');
+  const ul = document.createElement('ul');
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+  menu.classList.add('menu');
 
-  Pass the function an array as it's only argument.
+  menuItems.forEach( menuItem => {
+    const li = document.createElement('li');
+    li.textContent = menuItem;
+    ul.appendChild(li);
+  })
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
+menu.appendChild(ul);
 
-  Step 3: Using a DOM selector, select the menu button currently on the DOM.
+const menuButton = document.querySelector('.menu-button')
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+})
+return menu;
+
+}
+
+const menu = createMenuComponent(menuItems);
+
+const header = document.querySelector('.header');
+header.appendChild(menu);
+
+  //Step 1: Write a function that will create a menu component as seen below:
+
+  //<div class="menu">
+    //<ul>
+      //{each menu item as a list item}
+    //</ul>
+  //</div>
+
+  //Pass the function an array as it's only argument.
+
+  //Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  //Add those items to the <ul>
+
+  //Step 3: Using a DOM selector, select the menu button currently on the DOM.
   
-  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+  //Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
-  Step 5: return the menu component.
+  //Step 5: return the menu component.
 
-  Step 6: add the menu component to the DOM.
+  //Step 6: add the menu component to the DOM.
   
-*/
+
